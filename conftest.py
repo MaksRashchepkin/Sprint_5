dda3063d0b870853f4c4054c8bf9dab9ebffef1c
Maks Rashchepkin <m.rashepkin2018@gmail.com>
2024-11-20 11:08:36 +0300

    Sprint_5

--- conftest.py
+++ conftest.py
@@ -1,11 +1,27 @@
 import pytest
 from selenium import webdriver
-from data import Url
+import time
+from locators import Locators
+from data import Data, Url
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.get(Url.main_page)
+    time.sleep(3)
     yield driver
     driver.quit()
+
+@pytest.fixture
+def login(driver):
+    driver.get(Url.login_page)
+    driver.find_element(*Locators.input_email).send_keys(Data.email)
+    driver.find_element(*Locators.input_password).send_keys(Data.password)
+    driver.find_element(*Locators.login_button_login_page).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located(Locators.order_button))
+    assert driver.find_element(*Locators.order_button).is_enabled()
+    return login
+
